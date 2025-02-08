956c750e7b918d3bcccfce82d3080aadca754385
Rokon Khan <rokonjust@gmail.com>
2025-02-08 16:31:08 +0600

    The Swiss Moto Server Setup Done

---
+++ index.js
@@ -0,0 +1,139 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+const app = express();
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const morgan = require("morgan");
+
+// CORS configuration
+const corsOptions = {
+  origin: ["http://localhost:5173", "https://edu-management-system.surge.sh"],
+  credentials: true, // Allows credentials like cookies to be sent
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use CORS with the specified options
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+// Middleware to verify token
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err);
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hwao6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+
+    const database = client.db("swissMoto");
+    const usersCollection = database.collection("users");
+    const eventsCollection = database.collection("classes");
+
+    // Generate jwt token
+    app.post("/jwt", async (req, res) => {
+      const email = req.body;
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "365d",
+      });
+      res
+        .cookie("token", token, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+        })
+        .send({ success: true });
+    });
+
+    // Logout
+    app.get("/logout", async (req, res) => {
+      try {
+        res
+          .clearCookie("token", {
+            maxAge: 0,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+          })
+          .send({ success: true });
+      } catch (err) {
+        res.status(500).send(err);
+      }
+    });
+
+    // Add Class in Database
+
+    app.post("/events", verifyToken, async (req, res) => {
+      const eduClass = req.body;
+      const result = await eventsCollection.insertOne(eduClass);
+      res.send(result);
+    });
+
+    // save or update a user in db
+    app.post("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const user = req.body;
+      // check if user exists in db
+      const isExist = await usersCollection.findOne(query);
+      if (isExist) {
+        return res.send(isExist);
+      }
+      const result = await usersCollection.insertOne({
+        ...user,
+        role: "eventManager",
+        timestamp: Date.now(),
+      });
+      res.send(result);
+    });
+
+    // Get user From Database
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", async (req, res) => {
+  res.send("Swiss Moto Server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Swiss Moto Crud Server is Running on Port: ${port}`);
+});
