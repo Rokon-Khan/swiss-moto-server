5e5bdf11bab8d98ccec03a0fe54ea432d0bcc2c6
Rokon Khan <rokonjust@gmail.com>
2025-02-08 19:39:14 +0600

    Event POST and Get Method Added

--- index.js
+++ index.js
@@ -87,11 +87,17 @@ async function run() {
       }
     });
 
-    // Add Class in Database
+    // Add Events in Database
 
     app.post("/events", verifyToken, async (req, res) => {
-      const eduClass = req.body;
-      const result = await eventsCollection.insertOne(eduClass);
+      const events = req.body;
+      const result = await eventsCollection.insertOne(events);
+      res.send(result);
+    });
+
+    // get all Events from db
+    app.get("/events", async (req, res) => {
+      const result = await eventsCollection.find().toArray();
       res.send(result);
     });
 
@@ -118,6 +124,13 @@ async function run() {
       const cursor = usersCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    // get user role
+    app.get("/users/role/:email", async (req, res) => {
+      const email = req.params.email;
+      const result = await usersCollection.findOne({ email });
+      res.send({ role: result?.role });
     });
 
     // await client.db("admin").command({ ping: 1 });
